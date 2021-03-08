a97342c2ed05bdf453373e53d2072e4630731174
Thomas <lefoux82@gmail.com>
2021-03-08 17:12:56 +0100

    form for add a new transaction

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  //Intencie
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx,);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Titre',
+              ),
+              controller: titleController,
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Prix',
+              ),
+              controller: amountController,
+              // onChanged: (value) => amountInput = value,
+            ),
+            FlatButton(
+              child: Text(
+                'Ajouter une dépense',
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 14,
+                ),
+              ),
+              onPressed: () {
+                print(titleController.text);
+                print(amountController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text));
+              },
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
