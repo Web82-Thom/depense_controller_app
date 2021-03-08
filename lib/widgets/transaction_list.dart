bcd380f6cbeb3c143b13a7f23e4d60147e757cc7
Thomas <lefoux82@gmail.com>
2021-03-08 17:10:22 +0100

    mouve listTransaction in user_transaction

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,81 +1,68 @@
+import 'package:depense_controller_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  _TransactionListState createState() => _TransactionListState();
-}
+class TransactionList extends StatelessWidget {
+  //recuperer le transactions
+  final List<Transaction> transactions;
 
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 'T1',
-      title: 'Nouvelle chaussure',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 'T2',
-      title: 'Téléphone',
-      amount: 150,
-      date: DateTime.now(),
-    ),
-  ];
+  //Instencie la list
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return  Column(
-            children: _userTransactions.map((tx) {
-              //display one transaction
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    //Price
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      child: Text(
-                        tx.amount.toString() + '€', //pour le $ '\$${tx.amount}'
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    //Title et date
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMEd().format(tx.date),
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+      children: transactions.map((tx) {
+        //display one transaction
+        return Card(
+          child: Row(
+            children: <Widget>[
+              //Price
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
                 ),
-              );
-            }).toList(),
-          );
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.purple,
+                  width: 2,
+                )),
+                child: Text(
+                  tx.amount.toString() + '€', //pour le $ '\$${tx.amount}'
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              //Title et date
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    DateFormat.yMEd().format(tx.date),
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
