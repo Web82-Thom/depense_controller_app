902262416d497412704e56273b8d892eae43f10a
Thomas <lefoux82@gmail.com>
2021-03-08 17:42:34 +0100

    refactorie in container and widget to scroll Function

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,57 +12,62 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      children: transactions.map((tx) {
-        //display one transaction
-        return Card(
-          child: Row(
-            children: <Widget>[
-              //Price
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                child: Text(
-                  tx.amount.toString() + '€', //pour le $ '\$${tx.amount}'
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
-              //Title et date
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return  Container(
+      height: 300,
+      child: SingleChildScrollView(
+              child: Column(
+          children: transactions.map((tx) {
+            //display one transaction
+            return Card(
+              child: Row(
                 children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  //Price
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    child: Text(
+                      tx.amount.toString() + '€', //pour le $ '\$${tx.amount}'
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat.yMEd().format(tx.date),
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey[400],
-                    ),
-                  ),
+                  //Title et date
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMEd().format(tx.date),
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
