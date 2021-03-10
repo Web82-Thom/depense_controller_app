eec197c325eee54b15512dc767f58c8b78139faa
Thomas <lefoux82@gmail.com>
2021-03-10 17:34:30 +0100

    clean

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -26,7 +26,7 @@ class ChartBar extends StatelessWidget {
             children: <Widget>[
               Container(
                 decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey,width:1.0,),
+                  border: Border.all(color: Colors.grey, width:1.0,),
                   color: Color.fromRGBO(220, 220, 220, 1),
                   borderRadius: BorderRadius.circular(10), 
                   ),
