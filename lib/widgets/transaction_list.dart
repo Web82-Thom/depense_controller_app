c713502333631fc75da4852541eab1778e1791c6
Thomas <lefoux82@gmail.com>
2021-03-15 15:06:15 +0100

    clean the code and call widget TransactionItem

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   //recuperer le transactions
   final List<Transaction> transactions;
   final Function deleteTx;
-
   //Instencie la list et function
   TransactionList(this.transactions, this.deleteTx);
 
@@ -15,85 +14,34 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx, constraints) {
-            return Column(children: <Widget>[
-              Text(
-                'Pas de transaction!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              //Utiliser généralement en espacement
-              SizedBox(
-                height: 20,
-              ),
-              //Mettre l'image dans un container
-              Container(
-                height: constraints.maxHeight * 
-                0.5,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                )
-              ),
-            ],
-            );
-          })
-           
-          : ListView.builder(
-              itemBuilder: (contexte, index) {
-                return Card(
-                  elevation: 6,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 10,
-                  ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child : Padding(
-                          padding: EdgeInsets.all(6),
-                          child: FittedBox(
-                            child: Text(
-                              transactions[index].amount.toStringAsFixed(2) +'€',
-                            ),
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        transactions[index].title,
-                        //Le style par rapport au theme predefini dans main.dart
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      subtitle: Text(
-                        DateFormat.yMEd('fr').format(transactions[index].date),
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      trailing: MediaQuery.of(context).size.width > 460 
-                      ? TextButton.icon(
-                        icon:  Icon(
-                          Icons.delete, 
-                          color: Theme.of(context).errorColor,
-                        ),
-                        label: Text(
-                          'Supprimer', 
-                          style: TextStyle(
-                          color: Theme.of(context).errorColor, 
-                          ),
-                        ),
-                        onPressed: () => deleteTx(transactions[index].id),
-                      )
-                      :IconButton(
-                        icon: Icon(Icons.delete), 
-                        color: Theme.of(context).errorColor,
-                        onPressed: () => deleteTx(transactions[index].id),
-                        ),
-                    ),
-                );
-              },
-              itemCount: transactions.length,
-            );
-    
+    ? LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: <Widget>[
+          Text(
+            'Pas de transaction!',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          //Utiliser généralement en espacement
+          SizedBox(
+            height: 20,
+          ),
+          //Mettre l'image dans un container
+          Container(
+            height: constraints.maxHeight * 
+            0.5,
+            child: Image.asset(
+              'assets/images/waiting.png',
+              fit: BoxFit.cover,
+            )
+          ),
+        ],
+      );
+    },)
+    : ListView.builder(
+        itemBuilder: (contexte, index) {
+          return TransactionItem(transaction: transactions[index], deleteTx: deleteTx,);
+        },
+        itemCount: transactions.length,
+    );
   }
 }
