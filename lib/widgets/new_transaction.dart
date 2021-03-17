3d6a2792d8f85a979619066cc852b7909b68c71d
Thomas <lefoux82@gmail.com>
2021-03-17 07:36:45 +0100

    put initState, didUpdateWidget, dispose for the widget Life

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -5,16 +5,51 @@ class NewTransaction extends StatefulWidget {
   //Intencie
   final Function addTx;
 
-  NewTransaction(this.addTx,);
+  NewTransaction(this.addTx,){
+    print('Constructor NewTransaction Widget 1');
+    
+  }
 
   @override
-  _NewTransactionState createState() => _NewTransactionState();
+  _NewTransactionState createState() {
+
+    print('createState NewTransaction Widget 2');
+    return _NewTransactionState();
+   
+    }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+
+  _NewTransactionState(){
+    print('constructor NewTransaction state');
+  }
+
+  //vie du widget initState/didUpdateWidget/dispose
+  @override
+  void initState() {
+    
+    print(' initState()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    
+    print(' didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    
+    print(' dispose()');
+    super.dispose();
+  }
+
 
   void _submitData() {
     if (_amountController.text.isEmpty) {
@@ -55,7 +90,6 @@ class _NewTransactionState extends State<NewTransaction> {
       });
       
     });
-    print('...');
   }
 
   @override
